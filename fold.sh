53e3f525aff56cea06e9a15eb2c62be38d458371
frankielp <96617665+frankielp@users.noreply.github.com>
2023-04-17 18:09:52 +0700

    final

---
+++ fold.sh
@@ -0,0 +1,50 @@
+cp 1/*.txt fold0/labels/train/
+cp 2/*.txt fold0/labels/train/
+cp 3/*.txt fold0/labels/train/
+cp 4/*.txt fold0/labels/train/
+
+cp 1/*.jpg fold0/images/train/
+cp 2/*.jpg fold0/images/train/
+cp 3/*.jpg fold0/images/train/
+cp 4/*.jpg fold0/images/train/
+
+cp 0/*.txt fold1/labels/train/
+cp 2/*.txt fold1/labels/train/
+cp 3/*.txt fold1/labels/train/
+cp 4/*.txt fold1/labels/train/
+
+cp 0/*.jpg fold1/images/train/
+cp 2/*.jpg fold1/images/train/
+cp 3/*.jpg fold1/images/train/
+cp 4/*.jpg fold1/images/train/
+
+cp 0/*.txt fold2/labels/train/
+cp 1/*.txt fold2/labels/train/
+cp 3/*.txt fold2/labels/train/
+cp 4/*.txt fold2/labels/train/
+
+cp 0/*.jpg fold2/images/train/
+cp 1/*.jpg fold2/images/train/
+cp 3/*.jpg fold2/images/train/
+cp 4/*.jpg fold2/images/train/
+
+cp 0/*.txt fold3/labels/train/
+cp 1/*.txt fold3/labels/train/
+cp 2/*.txt fold3/labels/train/
+cp 4/*.txt fold3/labels/train/
+
+cp 0/*.jpg fold3/images/train/
+cp 1/*.jpg fold3/images/train/
+cp 2/*.jpg fold3/images/train/
+cp 4/*.jpg fold3/images/train/
+
+cp 0/*.txt fold4/labels/train/
+cp 1/*.txt fold4/labels/train/
+cp 2/*.txt fold4/labels/train/
+cp 3/*.txt fold4/labels/train/
+
+cp 0/*.jpg fold4/images/train/
+cp 1/*.jpg fold4/images/train/
+cp 2/*.jpg fold4/images/train/
+cp 3/*.jpg fold4/images/train/
+
